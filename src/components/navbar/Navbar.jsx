9ddfdf26fa9ab16ad9f5f23e8351108e1d6706a2
prsndp <prasandeepp@gmail.com>
2022-12-17 23:25:20 +0530

    topbare created!

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,15 +5,35 @@ import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
 import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import {Link} from 'react-router-dom';
+
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className="left">
-            <Link to='/' style={{ TextDecoration: "none" }}>
+            <Link to='/' style={{ textDecoration: 'none' }}>
+                
                 <span>SocialPedia</span>
             </Link>
+                <HomeOutlinedIcon />
+                <DarkModeOutlinedIcon />
+                <GridViewOutlinedIcon />
+            <div className='search'>
+                <SearchOutlinedIcon />
+                <input type='text' placeholder='search' />
+            </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+            < PersonOutlineOutlinedIcon />
+            < EmailOutlinedIcon />
+            < NotificationsOutlinedIcon />
+            <div className='user'>
+                <img src='Joh Doe' alt='' />
+                <span> John Doe</span>
+            </div>
+        </div>
     </div>
   )
 }
