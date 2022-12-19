f4825630b48ae5009a732a690edd3b995b8da0e6
prsndp <prasandeepp@gmail.com>
2022-12-19 18:45:49 +0530

    left and right bar created!

--- src/components/leftbar/Leftbar.jsx
+++ src/components/leftbar/Leftbar.jsx
@@ -2,7 +2,62 @@ import React from 'react'
 import './leftbar.scss';
 const Leftbar = () => {
   return (
-    <div className='leftbar' >Leftbar</div>
+    <div className='leftbar' >
+        <div className='container'>
+            <div className="menu">
+                <div className="user">
+                    <img src='Joh Doe' alt='' />
+                    <span>John Doe</span>
+                </div>
+                 <div className="item">
+                    <img src="" alt="" />
+                    <span>Friends</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Groups</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Marketplace</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Watch</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Memories</span>
+                </div>
+              
+            </div>
+            <hr />
+            <div className="menu">
+                <span>Your Shortcuts</span>
+
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Friends</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Groups</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Marketplace</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Watch</span>
+                </div>
+                <div className="item">
+                    <img src="" alt="" />
+                    <span>Memories</span>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
